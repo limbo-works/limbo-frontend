d07a87ee0cf1e7cdda387c8f5d3b7abfbd0bb1b9
Saul Hardman <hello@iamsaul.co.uk>
2021-10-12 14:45:58 +0200

    refactor(screens): remove utilities + rename vars

--- packages/screens/src/index.js
+++ packages/screens/src/index.js
@@ -1,43 +1,29 @@
-const getMin = (em) => `${em}em`;
-const getMax = (em) => `${em - 0.01}em`;
-
 export default (values, rootFontSize = 16) =>
 	values.reduce(({ screens = {}, breakpoints = {} }, value) => {
-		const { value: pxValue, min = true, max = false } =
+		const { value: px, min: hasMin = true, max: hasMax = false } =
 			typeof value === 'number' ? { value } : value ?? {};
 
-		if (!pxValue) {
+		if (!px) {
 			throw new Error(`\`value\` required: ${value}`);
 		}
 
-		const emValue = pxValue / rootFontSize;
+		const em = px / rootFontSize;
+		const min = `${em}em`;
+		const max = `${em - 0.01}em`;
 
 		return {
 			screens: {
 				...screens,
 
-				...(min
-					? {
-							[`>=${pxValue}`]: getMin(emValue),
-					  }
-					: {}),
+				...(hasMin ? { [`>=${px}`]: min } : {}),
 
-				...(max
-					? {
-							[`<${pxValue}`]: { max: getMax(emValue) },
-					  }
-					: {}),
+				...(hasMax ? { [`<${px}`]: { max } } : {}),
 			},
 
 			breakpoints: {
 				...breakpoints,
 
-				[`${pxValue}`]: {
-					px: pxValue,
-					em: emValue,
-					min: getMin(emValue),
-					max: getMax(emValue),
-				},
+				[`${px}`]: { px, em, min, max },
 			},
 		};
 	}, {});
